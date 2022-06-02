914bdc3c1c4aab747b53b53e640a2f64c71a17ac
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-06-01 20:26:26 -0400

    commit tree 83d17c57702cd1b09a59cab701440b84cc3cd4d1

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }    return true; 
 }
 
 /* 
